5092f7517869cd5771f5a54896814a96bf7e5ec6
Sotaro Ando <sotaro.ando711@gmail.com>
2026-01-18 18:23:28 +0900

    2026/1/18

--- udemy/basic_algorithm/practice_1.py
+++ udemy/basic_algorithm/practice_1.py
@@ -1,3 +1,4 @@
+# ①
 
 # 選択ソート
 # 最小値を見つけ、先頭と交換
@@ -47,7 +48,6 @@
 #     else:
 #         arr[0] = tmp
 
-org_list = [17, 11, 12, 5, 14, 9, 6, 16, 4, 10, 1, 19, 13, 15, 0, 2, 3, 18, 7, 8]
 # insert_sort(org_list)
 # print(org_list)
             
@@ -65,3 +65,123 @@ org_list = [17, 11, 12, 5, 14, 9, 6, 16, 4, 10, 1, 19, 13, 15, 0, 2, 3, 18, 7, 8
 
 # bubble_sort(org_list)
 # print(org_list)
+
+# ②
+# 1, 1, ,2, 3, 5, 8,...
+# fib(n) = fib(n-1) + fib(n-2)
+# 与えられた問題を小さな問題に分割
+# 部分問題を解く
+# 合体する 
+
+# def fib(n):
+# 	if n == 1 or n == 0:
+# 		return 1
+# 	return fib(n-1) + fib(n-2)
+
+# for i in range(35):
+# 	print(fib(i))
+
+# 同じ問題を複数回計算しているので無駄が多い
+
+
+# めもかフィボナッチ
+# 計算した結果をmemo[]の保存
+# def memo_fib(n):
+# 	memo = [None] * (n+1)
+
+# 	def _fib(n):
+# 		if n == 0 or n == 1:
+# 			return 1
+# 		if memo[n] != None:
+# 			return memo[n]
+# 		memo[n] = _fib(n-1) + _fib(n-2)
+# 		return memo[n]
+# 	return _fib(n)
+
+# for i in range(35):
+# 	print(memo_fib(i))
+
+# マージソート
+# nこの要素を含む配列をそれぞれ n/2個に分割する(divide)
+# その二つの部分配列をそれぞれmerge_sortでソートする(solve)
+# 得られたソート済み部分配列をmergeで統合する(conquer)
+
+def merge_sort(arr):
+    if len(arr) < 2:
+        return arr
+    # 配列を二つに分割
+    mid = len(arr) // 2
+    # arr1, arr2 = arr[:mid], arr[mid:]
+    return merge(merge_sort(arr[:mid]), merge_sort(arr[mid:]))
+
+def merge(arrf, arrb):
+    if len(arrf) < 1:
+        return arrb
+    if len(arrb) < 1:
+        return arrf
+    if arrf[0] <= arrb[0]:
+        return [arrf[0]] + merge(arrf[1:], arrb)
+    else:
+        return [arrb[0]] + merge(arrf, arrb[1:])
+
+# print(merge_sort(org_list))
+
+# nこの要素はlog_2N回分割
+# 8=>4=>2=>1
+# 分割の数だけmergeが必要
+# O(nlogn)
+
+# ③
+# クイックソート
+# ピボットを基準に二つに分割(divide)
+# 前方の配列にquick_sort(solve)
+# 後方の配列にquick_sort(solve)
+# 整列部分とピボットを連結して返す(conquer)
+# org_list = [17, 11, 12, 5, 14, 9, 6, 16, 4, 10, 1, 19, 13, 15, 0, 2, 3, 18, 7, 8]
+
+# def quick_sort(arr):
+#     if len(arr) < 2:
+#         return arr
+#     p = arr[0]
+#     arrf, arrb = divide(p, arr[1:])
+#     return quick_sort(arrf) + [p] + quick_sort(arrb)
+
+# def divide(p, arr):
+#     left = []
+#     right = []
+#     for i in arr:
+#         if p > i:
+#             left.append(i)
+#         else:
+#             right.append(i)
+#     print(left, right)
+#     return left, right
+
+# print(quick_sort(org_list))
+
+# バケットソート
+# カウンタ配列を作る（カウンタ配列の長さは元の配列の長さの最大値）
+# ex) A: [1, 4, 2, 0, 3, 2, 2, 4] => [1, 1, 3, 1, 2]
+# カウンタ配列の累積和が、配列Aの要素の移動先になる
+# [1, 2, 5, 6, 8]
+#  [ , , , , , , , 4] => [1, 2, 5, 6, 7]
+
+org_list =  [17, 11, 11, 5, 14, 9, 6, 19, 4, 10, 1, 19, 13, 5, 0, 2, 13, 18, 7, 8]
+
+def bucket_sort(arr):
+    # カウンタ並列の作成
+    arrc = [0] * (max(arr) + 1)
+    for i in arr:
+        arrc[i] += 1
+    print("カウンタ配列", arrc)
+    # 累積和を求める
+    for j in range(1, len(arrc)):
+        arrc[j] = arrc[j] + arrc[j-1]
+    print("累積和", arrc)
+    # ソート先の配列を用意して一つずつ代入
+    arrs = [0] * len(arr)
+    for i in reversed(arr):
+        arrs[arrc[i]-1] = i
+        arrc[i] -= 1
+    return arrs
+print(bucket_sort(org_list))
