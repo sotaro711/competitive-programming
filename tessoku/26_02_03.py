1e6a4489a59caf02733fdc1f81827e668d5875e5
Sotaro Ando <sotaro.ando711@gmail.com>
2026-02-04 22:31:24 +0900

    2026/02/04

--- tessoku/26_02_03.py
+++ tessoku/26_02_03.py
@@ -31,20 +31,60 @@
 # for i in range(1, D+1):
     # print(Answer[i])
 
-N, X = map(int, input().split())
+# N, X = map(int, input().split())
+# A = list(map(int, input().split()))
+
+
+# left = 0
+# right = N-1
+# while left <= right:
+#     mid = (left + right) // 2
+#     # print(mid)
+#     if A[mid] == X:
+#         print(mid+1)
+#         break
+#     if X < A[mid]:
+#         right = mid - 1
+#     if X > A[mid]:
+#         left = mid + 1
+
+N = int(input())
 A = list(map(int, input().split()))
+Q = int(input())
+X = []
+for _ in range(Q):
+    x = int(input())
+    X.append(x)
 
+print("A", A)
+print("X", X)
 
-left = 0
-right = N-1
-while left <= right:
-    mid = (left + right) // 2
-    # print(mid)
-    if A[mid] == X:
-        print(mid+1)
-        break
-    if X < A[mid]:
-        right = mid - 1
-    if X > A[mid]:
-        left = mid + 1
+def sort(arr):
+    for i in range(1, N):
+        exchange(arr, i)
+    return arr
 
+def exchange(arr,i):
+    tmp = arr[i]
+    for j in range(i-1, -1, -1):
+        if arr[j] > tmp:
+            arr[j+1] = arr[j]
+        else:
+            arr[j+1] = tmp
+            break
+    else:
+        arr[0] = tmp
+
+sorted_arr = sort(A)
+
+# AにはXより小さい数字が何個ありますか？
+for i in X:
+    left = 0
+    right = len(sorted_arr)
+    while left < right:
+        mid = (left + right) // 2
+        if sorted_arr[mid] < i:
+            left = mid + 1
+        else:
+            right = mid
+    print(left)
