e7c028bbaa874a74299614d52323872414b307b5
Sotaro Ando <sotaro.ando711@gmail.com>
2026-05-08 10:52:39 +0900

    ナップザック

---
+++ tessoku/month05/A19.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+long long N, W, w[109], v[109], dp[109][100009];
+
+int main () {
+    scanf("%lld %lld", &N, &W);
+    for (int i = 1; i <= N; i++) {
+        scanf("%lld %lld", &w[i], &v[i]);
+    }
+
+    dp[0][0] = 0;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j <= W; j++){
+            if (j >= w[i]) {
+                if (dp[i-1][j] >= dp[i-1][j-w[i]] + v[i]) {
+                    dp[i][j] = dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j-w[i]] + v[i];
+                }
+            } else {
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+
+    printf("%lld", dp[N][W]);
+}
