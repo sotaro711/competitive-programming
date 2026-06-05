6c5b165988880852fb0b032089c091dbe2752918
Sotaro Ando <sotaro.ando711@gmail.com>
2026-06-05 17:28:06 +0900

    dfs

---
+++ tessoku/month06/A62.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int N, M, A[100009], B[100009];
+int main() {
+    cin >>  N >> M;
+    for (int i = 1; i <= M; i++) {
+        cin >> A[i] >> B[i];
+    }
+
+    // 連結か連結でないかをdfsで検索
+    
+}
