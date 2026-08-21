628cfc8990dff68ce2397df626fca6cd247067d1
Sotaro Ando <sotaro.ando711@gmail.com>
2026-08-21 14:25:39 +0900

    8/21

--- cpp/adt_easy_20260819_1/c/main.cpp
+++ cpp/adt_easy_20260819_1/c/main.cpp
@@ -1,7 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int N;
+string S[109];
+vector<pair<int, int>> ans;
+
 int main() {
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> S[i];
+    }
+
+    ans.resize(N+1);
+
+    for (int i = 1; i <= N; i++) {
+        ans[i].first = 0;
+        ans[i].second = i;
+    }
+
+    // 二次元配列で結果を保存？
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j < (int)S[i].size(); j++) {
+            if (S[i][j] == 'o') ans[i].first--;
+        }
+    }
+
+    sort(ans.begin()+1, ans.end());
+
+    for (int i = 1; i <= N; i++) {
+        cout << ans[i].second << " ";
+    }
+
+    cout << endl;
 
     return 0;
 }
