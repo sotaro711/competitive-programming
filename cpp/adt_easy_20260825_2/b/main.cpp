586e722e2a2190ea118dc71e8e594e6884e66b8a
Sotaro Ando <sotaro.ando711@gmail.com>
2026-08-27 11:26:43 +0900

    8/27

--- cpp/adt_easy_20260825_2/b/main.cpp
+++ cpp/adt_easy_20260825_2/b/main.cpp
@@ -2,6 +2,56 @@
 using namespace std;
 
 int main() {
+    int N;
+    cin >> N;
+    
+    string S;
+    cin >> S;
+
+    bool con1 = false;
+    if (N % 2 != 0) con1 = true;
+
+    bool con2 = true;
+    int tmp = (N + 1) / 2;
+    // for (int i = 0; i < tmp - 1; i++) {
+    //     if (S[i] != '1') {
+    //         con2 = false;
+    //         break;
+    //     } else {
+    //         con2 = true;
+    //     }
+    // }
+
+    // 1を見つけたらfalseにしてbreak
+
+    for (int i = 0; i < tmp - 1; i++) {
+        if (S[i] != '1') {
+            con2 = false;
+            break;
+        }
+    }
+
+    bool con3 = false;
+    if (S[tmp - 1] == '/') con3 = true;
+    bool con4 = true;
+    // for (int i = tmp; i < N; i++) {
+    //     if (S[i] != '2') {
+    //         con4 = false;
+    //         break;
+    //     } else {
+    //         con4 = true;
+    //     }
+    // }
+
+    for (int i = tmp; i < N; i++) {
+        if (S[i] != '2') {
+            con4 = false;
+            break;
+        }
+    }
+
+    if (con1 && con2 && con3 && con4) cout << "Yes" << endl; else cout << "No" << endl;
+    
 
     return 0;
 }
--- cpp/tessoku-book/a08/main.cpp
+++ cpp/tessoku-book/a08/main.cpp
@@ -2,6 +2,64 @@
 using namespace std;
 
 int main() {
+    int H, W;
+    cin >> H >> W;
+
+    vector<vector<int>> X(H+1, vector<int>(W+1, 0));
+    for (int i = 1; i <= H; i++) {
+        for (int j = 1; j <= W; j++) {
+            cin >> X[i][j];
+        }
+    }
+
+    int Q;
+    cin >> Q;
+
+    vector<int> A(Q+1), B(Q+1), C(Q+1), D(Q+1);
+    for (int i = 1; i <= Q; i++) {
+        cin >> A[i] >> B[i] >> C[i] >> D[i];
+    }
+
+    // 直接。。？
+    // for (int i = 1; i <= Q; i++) {
+    //     int sum = 0;
+    //     for (int j = A[i]; j <= C[i]; j++) {
+    //         for (int k = B[i]; k <= D[i]; k++) {
+    //             sum += X[j][k];
+    //         }
+    //     }
+    //     cout << sum << endl;
+    // }
+    // 計算量はO(HWQ)
+
+    // これだと時間が足りない
+    // 効率的にやるべき
+    // 行の累積和を求めたあと、列の累積和を求める
+    // その箇所の累積和から不要なところを引く
+
+    // 行列の累積和を作る
+    // 行の累積和
+    for (int i = 1; i <= H; i++) {
+        for (int j = 1;  j <= W; j++) {
+            X[i][j] += X[i][j-1];
+        }
+    }
+
+    // 列の累積和
+    for (int i = 1; i <= H; i++) {
+        for (int j = 1; j <= W; j++) {
+            X[i][j] += X[i-1][j];
+        }
+    }
+
+    // 累積和から計算
+    for (int i = 1; i <= Q; i++) {
+        int ans = X[C[i]][D[i]] + X[A[i] - 1][B[i] - 1] - X[A[i] - 1][D[i]] - X[C[i]][B[i] - 1];
+        cout << ans << endl;
+    }
+
+    // 計算量はO(HW)
+    // 全体ではO(HW + Q)
 
     return 0;
 }
